a2406cb9c881c1a2b7c12726515e88cb5a331d4f
t-linguist <92468855+t-linguist@users.noreply.github.com>
2022-11-13 12:20:23 +0300

    Delete test_toxic.py

--- test_toxic.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-
-classifier = pipeline('text-classification',
-                      model='SkolkovoInstitute/russian_toxicity_classifier')
-
-print(classifier('Я обожаю инженерию машинного обучения!'))
