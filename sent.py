2feb3f96a9c0a5aa2b65242b21f949c428389441
t-linguist <92468855+t-linguist@users.noreply.github.com>
2022-11-19 14:30:45 +0300

    Delete sent.py

--- sent.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-classifier = pipeline('sentiment-analysis',
-                      model='blanchefort/rubert-base-cased-sentiment')
-
-output = classifier(input())
-
-print(output)
